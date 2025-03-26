a60f2b9ec85e5f9392e8a73c49d638052ac9954b
saraiya-rishi <saraiyarishi8102@gmail.com>
2025-03-26 13:07:35 +0530

    rishi history

---
+++ Controllers/Restaurant/OrderHistory.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Foodie.Controllers.Restaurant
+{
+    public class OrderHistory : Controller
+    {
+        public IActionResult History()
+        {
+            return View();
+        }
+    }
+}
